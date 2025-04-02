39e81bded148f2612351cb2dd0151f92f8e8a6b7
X-Y-Z-QWERTY <dmitrykutshinski@gmail.com>
2025-04-02 13:22:50 +0300

    finish noREST app from tutorial

--- src/main/java/com/melodych/site_with_tests/DemoApplication.java
+++ src/main/java/com/melodych/site_with_tests/DemoApplication.java
@@ -2,8 +2,8 @@ package com.melodych.site_with_tests;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
